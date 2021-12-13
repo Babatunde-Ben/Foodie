cfce1ecf905f452f143350b713bfd57cb08a4eb1
Babatude-Ben <babatundeben110gmail.com>
2021-12-13 03:04:46 -0800

    modified search functionality

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const container = document.querySelector(".container");
 let url = `https://api.spoonacular.com/recipes/random?number=9&tags=vegetarian&apiKey=838b6353edcb4753a360558332db1166`;
+
+// ------ for front page ------
+
 function fetchFood(url) {
   fetch(url)
     .then((res) => res.json())
@@ -12,10 +15,6 @@ function fetchFood(url) {
       console.log(error);
     });
 }
-
-// generate food from spononacular API
-
-window.addEventListener("DOMContentLoaded", fetchFood(url));
 
 function generateFood(fetchArray) {
   const content = fetchArray.map(function (item) {
@@ -37,6 +36,72 @@ function generateFood(fetchArray) {
   });
   container.innerHTML = content.join("");
 }
+
+// generate food from spononacular API
+
+window.addEventListener("DOMContentLoaded", fetchFood(url));
+
+//------ for search page ------
+
+function fetchFoodSearch(url) {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      const searchResult = data.searchResults[0].results;
+      // const result = data.recipes;
+      console.log(searchResult);
+      generateFoodSearch(searchResult);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function generateFoodSearch(fetchArray) {
+  const content = fetchArray.map(function (item) {
+    const { image, name, link, content } = item;
+    return ` <a href="${link}" target="_blank"><div class="food-container">
+    <img
+      src="${image}"
+      class="img"
+      alt="${name}"
+    />
+    <h2 class="food-title">${name}</h2>
+    <p class="desc">
+    ${content} 
+    </p>
+  </div></a>
+  
+  `;
+  });
+  container.innerHTML = content.join("");
+}
+
+// JS for search functionality
+
+const form = document.querySelector(".search-form");
+const searchBox = document.querySelector(".search-box");
+const searchResultText = document.querySelector(".search-result-text");
+// const searchResultValue = document.getElementById("search-result-value");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const searchValue = searchBox.value;
+  const container = document.querySelector(".container");
+  searchResultText.innerHTML = `search results for "${searchValue}"...`;
+  container.innerHTML = `<section class="waiting">
+    <div class="preloader">
+      <img src="./images/pre-loader.svg" alt="preloader" />
+    </div>
+    <p class="waiting-text">waiting for results...</p>
+  </section>`;
+  console.log(searchValue);
+
+  url = `https://api.spoonacular.com/food/search?query=${searchValue}&number=9&apiKey=838b6353edcb4753a360558332db1166`;
+
+  fetchFoodSearch(url);
+  form.reset();
+});
 
 // JS for responsive navbar
 
